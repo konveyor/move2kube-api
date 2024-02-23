9ac45ae30732f8a797db83a2cb2274b7349f8d23
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2024-02-23 20:00:50 +0530

    fixup! fixup! fix: better error message when the max upload size is exceeded
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- internal/move2kubeapi/handlers/inputs.go
+++ internal/move2kubeapi/handlers/inputs.go
@@ -18,6 +18,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -53,11 +54,12 @@ func HandleCreateProjectInput(w http.ResponseWriter, r *http.Request, isCommon b
 	}
 	r.Body = http.MaxBytesReader(w, r.Body, common.Config.MaxUploadSize)
 	if err := r.ParseMultipartForm(common.Config.MaxUploadSize); err != nil {
-		if _, ok := err.(*http.MaxBytesError); ok {
+		maxErr := &http.MaxBytesError{}
+		if ok := errors.As(err, &maxErr); ok {
 			logrus.Errorf("request body exceeded max upload size of '%d' bytes. Error: %q", common.Config.MaxUploadSize, err)
 			sendErrorJSON(
 				w,
-				"Request body exceeded max upload size. Try using a smaller input or contact your Admin to increase the max upload size.",
+				"Request body exceeded max upload size. Try using a smaller input. Use the '--max-upload-size' flag while starting the server to increase the limit.",
 				http.StatusBadRequest,
 			)
 			return
